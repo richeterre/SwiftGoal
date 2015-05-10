238aad28ead6b51a4bc4a3dbf79842dbc68193d3
Martin Richter <martin.richter@futurice.com>
2015-05-10 23:34:34 +0200

    Create empty Matches view controller

--- SwiftGoal/AppDelegate.swift
+++ SwiftGoal/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
 
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
+
+        window?.rootViewController = MatchesViewController()
         window?.makeKeyAndVisible()
 
         return true
---
+++ SwiftGoal/Views/MatchesViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MatchesViewController.swift
+//  SwiftGoal
+//
+//  Created by Martin Richter on 10/05/15.
+//  Copyright (c) 2015 Martin Richter. All rights reserved.
+//
+
+import UIKit
+
+class MatchesViewController: UITableViewController {
+
+}
